f4eb3da9ae5ba69cebe5170d106a40a31c867c11
cassiuschen <chzsh1995@gmail.com>
2016-07-26 10:37:21 -0700

    add plugins to git

---
+++ Assets/Plugins/Block.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+[System.Serializable]
+public class Block : System.Object {
+	public int time;
+	public int type;
+	public string pointImg;
+	public float top;
+	public float left;
+}
---
+++ Assets/Plugins/Level.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+[System.Serializable]
+public class Level : System.Object {
+	public string name;
+	public string audio;
+	public string previewAudio;
+	public string sketch;
+	public Block[] blocks;
+
+	public static Level CreateFromJSON(string jsonString) {
+		return JsonUtility.FromJson<Level> (jsonString);
+	}
+}
